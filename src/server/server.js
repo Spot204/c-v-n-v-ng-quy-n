32f17a1a10b92c7d7f1d5cc355a045e3598a3772
Lord-Eternal <et3rnxl24@gmail.com>
2025-09-16 12:26:31 +0700

    bach

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/connectDB.js";
+import logIn from "./routes/logIn.js";
 import createAccount from "./routes/createAccount.js";
 
 dotenv.config();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.use("/api/createAccount", createAccount);
+app.use("/api/logIn", logIn);
 
 
 app.listen(PORT, () => {
